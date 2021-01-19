e5790c3fe9240220dfaf710347bef5329d0666b1
anhba817 <dong.ng.tan@gmail.com>
2021-01-19 23:38:11 +0700

    Change API_URL to heroku URL

--- app/javascript/constants/common.js
+++ app/javascript/constants/common.js
@@ -2,4 +2,5 @@ export const SET_CSRF_TOKEN = "SET_CSRF_TOKEN";
 export const CLEAR_CSRF_TOKEN = "CLEAR_CSRF_TOKEN";
 
 export const CSRF_LOCAL_STORAGE_ID = "khungdienngo";
-export const API_URL = "http://localhost:3000/api";
+//export const API_URL = "http://localhost:3000/api";
+export const API_URL = "https://warm-plains-55086.herokuapp.com/api";
