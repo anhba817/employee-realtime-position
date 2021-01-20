cdd5b2be0e172811197a08a5704671929b8149e2
anhba817 <dong.ng.tan@gmail.com>
2021-01-20 22:20:12 +0700

    Change rails_blob_url to url_for

--- app/serializers/map_serializer.rb
+++ app/serializers/map_serializer.rb
@@ -6,7 +6,7 @@ class MapSerializer < ActiveModel::Serializer
   def image
     if object.image.attached?
       {
-        url: rails_blob_url(object.image)
+        url: url_for(object.image)
       }
     end
   end
